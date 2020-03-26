c675b98855e5cbafce825683092a29340784b94a
lganzzzo <lganzzzo@gmail.com>
2020-03-27 01:01:09 +0200

    Add License

--- server/src/rooms/Room.hpp
+++ server/src/rooms/Room.hpp
@@ -1,3 +1,28 @@
+/***************************************************************************
+ *
+ * Project:   ______                ______ _
+ *           / _____)              / _____) |          _
+ *          | /      ____ ____ ___| /     | | _   ____| |_
+ *          | |     / _  |  _ (___) |     | || \ / _  |  _)
+ *          | \____( ( | | | | |  | \_____| | | ( ( | | |__
+ *           \______)_||_|_| |_|   \______)_| |_|\_||_|\___)
+ *
+ *
+ * Copyright 2020-present, Leonid Stryzhevskyi <lganzzzo@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ***************************************************************************/
 
 #ifndef ASYNC_SERVER_ROOMS_ROOM_HPP
 #define ASYNC_SERVER_ROOMS_ROOM_HPP
